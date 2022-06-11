c2462838230ac2d0da0eacb30fbb23dbbc14c9ae
Arleys Gatica <gatica.arleys16@gmail.com>
2022-06-10 18:39:37 -0600

    Fachada del perfil

--- src/Components/Profile-Doctor/Profile.js
+++ src/Components/Profile-Doctor/Profile.js
@@ -1,25 +1,139 @@
 import React from 'react';
-import { View, Text, ScrollView, Image } from 'react-native';
-import estilos from './ProfileStyle';
+import { View, Text, ScrollView, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
 const Profile = () => {
+
     return (
-        <>
-            <ScrollView style={estilos.Container}>
-                <View style={estilos.ContainnerProfile}>
-                <Text> DR. ARLEYS </Text>
-                    <Text>Dermatologist</Text>
-                    </View>
-                <View>
-                <Image
-                        source={require('../../Asset/user.png')}
-                        style={estilos.ContainerJoin}    />
-                    <Text>4.5/5</Text>
+        <View style={styles.card}>
+            <View style={styles.cardHeader}>
+                <View style={styles.cardHeaderLeft}>
+                    <Image source={require('../../Asset/Logo.png')} style={styles.cardHeaderLeftImage} />
+                    <Text style={styles.cardHeaderLeftText}>Dr. John Doe</Text>
                 </View>
-                
-            </ScrollView>    
-        </>
+                <View style={styles.cardHeaderRight}>
+                    <Text style={styles.cardHeaderRightText}>5.0</Text>
+                    <Image source={require('../../Asset/Logo.png')} style={styles.cardHeaderRightImage} />
+                </View>
+            </View>
+            <View style={styles.cardBody}>
+                <View style={styles.cardBodyLeft}>
+                    <Text style={styles.cardBodyLeftText}>Cardiology</Text>
+                </View>
+
+            </View>
+            <View style={styles.cardFooter}>
+
+                <View style={styles.cardFooterRight}>
+                    <TouchableOpacity style={styles.cardFooterRightButton}>
+                        <Text style={styles.cardFooterRightButtonText}>
+                            Reservation
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View >
+                <Text style={styles.cardFooterRightText}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </Text>
+
+
+            </View>
+        </View>
+
     )
 }
+
+const styles = StyleSheet.create({
+    card: {
+        height: '40%',
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,
+        elevation: 1,
+        marginBottom: 10,
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
+        padding: 10,
+    },
+    cardHeader: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    cardHeaderLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    cardHeaderLeftImage: {
+        width: 85,
+        height: 85,
+        borderRadius: 15,
+        marginRight: 10,
+    },
+    cardHeaderLeftText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    cardHeaderRight: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    cardHeaderRightText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginRight: 10,
+    },
+    cardHeaderRightImage: {
+        width: 20,
+        height: 20,
+        marginRight: 10,
+    },
+    cardBody: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+
+        padding: 10,
+        marginTop: 10,
+    },
+    cardBodyLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    cardBodyLeftText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginRight: 10,
+    },
+
+    cardFooter: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+        padding: 10,
+        marginTop: 10,
+    },
+
+    cardFooterRight: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    cardFooterRightButton: {
+        backgroundColor: '#00a8ff',
+        borderRadius: 10,
+        padding: 10,
+        marginRight: 10,
+    },
+    cardFooterRightButtonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+    }
+})
 
 export default Profile;
