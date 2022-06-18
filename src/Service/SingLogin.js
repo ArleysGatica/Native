f0ca0aa9025ad54706124f333be7b5eb52ba830e
Arleys Gatica <gatica.arleys16@gmail.com>
2022-06-17 19:04:17 -0600

    SingLogin

--- src/Service/SingLogin.js
+++ src/Service/SingLogin.js
@@ -1,58 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import auth from '@react-native-firebase/auth';
-import { View, Text, ScrollView, Button, StatusBar, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, Button, StatusBar, TouchableOpacity, TextInput, Image } from 'react-native';
 
 const SingLogin = () => {
 
-    GoogleSignin.configure({
-        webClientId: '663908802543-p4no9ub7j6l3no76r92effc9jiucg4n2.apps.googleusercontent.com',
-
+    useEffect(() => {
+        const config = {
+            ClientId: "663908802543-p4no9ub7j6l3no76r92effc9jiucg4n2.apps.googleusercontent.com",
+            scopes: ["profile", "email"]
+        };
+        GoogleSignin.configure(config);
     });
 
-    const signIn = async () => {
+    const [user, setUser] = useState(null);
 
+    const signIn = async () => {
         try {
             await GoogleSignin.hasPlayServices();
             const userInfo = await GoogleSignin.signIn();
-            const credential = auth.GoogleAuthProvider.credential(userInfo.idToken, userInfo.accessToken);
-            const firebaseUserCredential = await auth().signInWithCredential(credential);
-            console.log(firebaseUserCredential.user.toJSON());
+            setUser(userInfo);
+            console.log(userInfo);
         } catch (error) {
-            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-                // user cancelled the login flow
-            } else if (error.code === statusCodes.IN_PROGRESS) {
-                // operation (f.e. sign in) is in progress already
-            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-                // play services not available or outdated
-            } else {
-                // some other error happened
-            }
+            console.log(error);
         }
     }
 
     const signOut = async () => {
         try {
+
             await GoogleSignin.revokeAccess();
             await GoogleSignin.signOut();
-            console.log("out");
+            setUser(null);
         } catch (error) {
-            console.error(error);
+            console.log(error);
         }
     }
 
     return (
         <ScrollView>
-            <StatusBar barStyle="dark-content" />
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>SingLogin</Text>
-                <Button title="Sign In" onPress={signIn} />
-                <Button title="Sign Out" onPress={signOut} />
+            <StatusBar backgroundColor="#fff" barStyle="dark-content" />
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontSize: 30, fontWeight: 'bold', color: '#000' }}>Sing In</Text>
+                <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#000' }}>Google</Text>
+                <TouchableOpacity onPress={signIn}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'red' }}>Sign In</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={signOut}>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold', color: 'blue' }}>Sign Out</Text>
+                </TouchableOpacity>
+                {user && (
+                    <View>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#000' }}>Name: {user.user.name}</Text>
+                        <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#000' }}>Email: {user.user.email}</Text>
+                        <Image source={{ uri: user.user.photo }} style={{ width: 100, height: 100 }} />
+                    </View>
+                )}
             </View>
-            
-        </ScrollView>
-    )
-
+        </ScrollView >
+    );
 }
 export default SingLogin;
 
