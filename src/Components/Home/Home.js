6d130f012d863521e29dbb4ee28339320aff1496
Arleys Gatica <gatica.arleys16@gmail.com>
2022-06-25 23:14:48 -0600

    Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,62 +2,74 @@ import React from 'react';
 import { Text, View, ScrollView, Image, TextInput, TouchableOpacity } from 'react-native';
 import estilos from './HomeStyle';
 import { HomeCard } from './HomeCard';
+import Doctor from '../../Service/Doctor';
+import ServiceSpecialities from '../../Service/Speciality';
 
-const Home = ({navigation}) => {
-        
-  return (
-    <ScrollView>
-      <View style={estilos.Header}>
-        <Image
-          source={require('../../Asset/sidebar.png')}
-          style={estilos.sidebar}
-        />
-        <Image
-          source={require('../../Asset/user.png')}
-          style={estilos.sidebar}
-        />
-      </View>
-      <View style={estilos.base}>
-        <Text style={estilos.Title}>Hello, Good Afternoon</Text>
-        <Text style={estilos.name}>Auron_Play !!</Text>
-      </View>
-      <View style={estilos.Container_Search}>
-        <View style={estilos.Container_Search_inside}>
-          <Text style={estilos.text}>
-            Find The Doctor Nearest To Your Location
-          </Text>
-          <View style={estilos.Container_items}>
-            <TextInput style={estilos.input} placeholder="Search" />
-            <Image
-              source={require('../../Asset/lupa.png')}
-              style={estilos.search}
-            />
-          </View>
-        </View>
-      </View>
-      <View style={estilos.Container_Category}>
-        <Text style={estilos.Container_Category_Info_Cat}>Category</Text>
-        <Text style={estilos.Container_Category_Info_sell}>See All</Text>
-          </View>
-          <TouchableOpacity onPress={() => navigation.navigate('ServiceSpecialities')}>
-      <View style={estilos.Container_Category_items}>
-        <View style={estilos.Container_Category_Item}>
-          <Image
-            source={require('../../Asset/Sing.png')}
-            style={estilos.Container_Category_items_img}
-          />
-          <Text style={estilos.Container_Category_items_text}>
-            Specialities
-          </Text>
-        </View>
-              </View>
-                </TouchableOpacity>
-          <View>
-              
-        <HomeCard />
-          </View>
+const Home = ({ route, navigation }) => {
+
+  //  console.log("user test ..... ", route.params);
+    const { user } = route.params;
+    console.log("user test ..... ", user.email);
+
+
+    // const [DataDOCTOR] = Doctor();
+    // const [DataSpecialities] = ServiceSpecialities();
+
+    return (
+        <ScrollView>
+            <View style={estilos.Header}>
+                <Image
+                    source={require('../../Asset/sidebar.png')}
+                    style={estilos.sidebar}
+                />
+                <Image
+                    source={require('../../Asset/user.png')}
+                    style={estilos.sidebar}
+                />
+            </View>
+            <View style={estilos.base}>
+                <Text style={estilos.Title}>Hello, Good Afternoon</Text>
+                <Text style={estilos.SubTitle}> ...</Text>
+                <Text style={estilos.name}>Auron_Play !!</Text>
+            </View>
+            <View style={estilos.Container_Search}>
+                <View style={estilos.Container_Search_inside}>
+                    <Text style={estilos.text}>
+                        Find The Doctor Nearest To Your Location
+                    </Text>
+                    <View style={estilos.Container_items}>
+                        <TextInput style={estilos.input} placeholder="Search" />
+                        <Image
+                            source={require('../../Asset/lupa.png')}
+                            style={estilos.search}
+                        />
+                    </View>
+                </View>
+            </View>
+            <View style={estilos.Container_Category}>
+                <Text style={estilos.Container_Category_Info_Cat}>Category</Text>
+                <Text style={estilos.Container_Category_Info_sell}>See All</Text>
+            </View>
+            <TouchableOpacity onPress={() => navigation.navigate('ServiceSpecialities')}>
+               
+                {/* {DataSpecialities.slice(0, 1)?.map((item) => (
+                    <View style={estilos.Container_Category_items}>
+                        <View style={estilos.Container_Category_Item}>
+                            <Image
+                                source={require('../../Asset/Logo.png')}
+                                style={estilos.Container_Category_items_img}
+                            />
+                            <Text style={estilos.Container_Category_items_text}>
+                                {item.name}
+                            </Text>
+                        </View>
+                    </View>
+                ))} */}
+            </TouchableOpacity>
+            <View>
          
-    </ScrollView>
-  );
+            </View>
+        </ScrollView>
+    );
 };
 export default Home;
