97b2a32aedf802d08ccb7e1db39eb537db2e4c4d
Arleys Gatica <gatica.arleys16@gmail.com>
2022-06-26 20:47:40 -0600

    speciality-god

--- src/Service/Speciality.js
+++ src/Service/Speciality.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import firestore from '@react-native-firebase/firestore';
 import Specialities from '../Pages/Specialities';
-import { View, Text, ScrollView, Image, FlatList } from 'react-native';
+import { View, Text, ScrollView, Image, FlatList, TouchableOpacity } from 'react-native';
 
-const ServiceSpecialities = () => {
+const ServiceSpecialities = ({ navigation }) => {
 
     const [DataSpecialities, setData] = useState([]);
 
@@ -14,11 +14,18 @@ const ServiceSpecialities = () => {
             .then(querySnapshot => {
                 const data = [];
                 querySnapshot.forEach(doc => {
-                    data.push(doc.data());
+                    data.push({
+                        id: doc.id,
+                        ...doc.data()
+                    })
                 });
                 setData(data);
             });
     }, []);
+
+    const cualquiera = (item) => {
+        navigation.navigate('Doctor', { Special: item });
+    }
 
     return (
         <>
@@ -31,38 +38,41 @@ const ServiceSpecialities = () => {
                         backgroundColor: '#08007C',
                         maxwidth: '100%',
                         maxHeight: '100%',
-                        borderRadius: 20,                       
+                        borderRadius: 20,
                     }}>
                     <FlatList
                         numColumns={2}
                         data={DataSpecialities}
                         renderItem={({ item }) => (
-                            <ScrollView
-                                style={{
-                                    marginHorizontal: 10,
-                                    marginVertical: 10,
-                                    backgroundColor: '#00B9E4',
-                                    alignSelf: 'center',
-                                    borderRadius: 20,
-                                }}>
-                                <Image
-                                    source={{ uri: item.image }}
-                                    resizeMode = 'contain'
-                                    style={{ width: 100, height: 100, margin: 10, alignSelf: 'center' }}
-                                />
-                                <Text
-                                    style={{ fontSize: 20, margin: 10, fontWeight: 'bold', alignSelf: 'center'  }}
-                                >{item.name}
-                                </Text>
-                            </ScrollView>
+                            <TouchableOpacity key={item.id} onPress={() => cualquiera(item)} >
+                                <ScrollView
+                                    style={{
+                                        marginHorizontal: 10,
+                                        marginVertical: 10,
+                                        backgroundColor: '#00B9E4',
+                                        alignSelf: 'center',
+                                        borderRadius: 20,
+                                    }}>
+                                    <Image
+                                        id={item.name}
+                                        source={{ uri: item.image }}
+                                        resizeMode='contain'
+                                        style={{ width: 100, height: 100, margin: 10, alignSelf: 'center' }}
+                                    />
+                                    <Text
+                                        style={{ fontSize: 20, margin: 10, fontWeight: 'bold', alignSelf: 'center' }}
+                                    >{item.name}
+
+                                    </Text>
+                                </ScrollView>
+                            </TouchableOpacity>
                         )}
-                            />
+                    />
                 </View>
             </View>
         </>
     )
-
 }
 
-export default ServiceSpecialities;
 
+export default ServiceSpecialities;
