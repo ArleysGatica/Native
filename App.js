78d026f5c069204192d89b6c8f32d349a0c4d9bb
Arleys Gatica <gatica.arleys16@gmail.com>
2022-06-27 21:52:32 -0600

    adding false-of-navUP

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Logo from './src/Components/Welcome/Logo';
@@ -9,16 +9,24 @@ import Profile from './src/Components/Profile-Doctor/Profile';
 import ReservationAgenda from './src/Components/Home/j';
 import SingLogin from './src/Service/SingLogin';
 import Doctor from './src/Service/Doctor';
+import Login from './src/Pages/Login/Login';
+import Specialities from './src/Pages/Specialities';
+import DoctorView from './src/Pages/Doctor';
+import Calendario from './src/Components/Calendar';
+import createBottomTabNavigator from './src/Components/Menu/AppMenu';
 
 const Stack = createNativeStackNavigator();
 
 function LogoStack() {
+   
     return (
         <Stack.Navigator>
-            <Stack.Screen name="LOGUITO" component={Logo} />
-            <Stack.Screen name="Welcome" component={Welcome} />
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="ServiceSpecialities" component={ServiceSpecialities} />
+            <Stack.Screen options={{ headerShown: false }} name="LOGUITO" component={Logo} />
+            <Stack.Screen options={{ headerShown: false }} name="Welcome" component={Welcome} />
+            <Stack.Screen options={{ headerShown: false }} name="Login" component={Login} />
+            <Stack.Screen options={{ headerShown: false }} name="Home" component={Home} />
+            <Stack.Screen options={{ headerShown: false }} name="ServiceSpecialities" component={ServiceSpecialities} />
+            <Stack.Screen options={{ headerShown: false }}name="Doctor" component={Doctor}  /> 
         </Stack.Navigator>
     )
 }
