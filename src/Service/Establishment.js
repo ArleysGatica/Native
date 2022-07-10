497363f086b3f249a08b3b615796fa3ee7380a0d
Arleys Gatica <gatica.arleys16@gmail.com>
2022-07-09 23:16:43 -0600

    Complet tood-app

--- src/Service/Establishment.js
+++ src/Service/Establishment.js
@@ -1,50 +1,79 @@
 import React, { useState, useEffect } from 'react';
 import firestore from '@react-native-firebase/firestore';
-import { View, Text, ScrollView, Image } from 'react-native';
+import { View, Text, ScrollView, Image, FlatList, TouchableOpacity} from 'react-native';
 
-const Establishments = ( props ) => {
+export const Establishments = ({ navigation, route }) => {
 
-    console.log(props);
+    const { user } = route.params;
+    
+    console.log("USER ESTABLISHMENT : ", user);
 
     const [data, setData] = useState([]);
 
     useEffect(() => {
         firestore()
-            .collection('Meetings')
+            .collection('Establishments')
             .get()
             .then(querySnapshot => {
                 const data = [];
                 querySnapshot.forEach(doc => {
-                    data.push(doc.data());
+                    data.push({
+                        id: doc.id,
+                        ...doc.data()
+                    })
                 });
                 setData(data);
             });
     }, []);
 
-    
+    const cualquiera = (item) => {
+        navigation.navigate('DoctorsEM', {establishment: item, user: user});
+    }
+
     return (
-        
-        <ScrollView>
-            <Text>User's</Text>
-            {
-                props.data.slice(0, 3).map((item, index) => {
-                    return (
-                        <View key={index}>
-                            <Image source={{ uri: item.image }} style={{ width: 100, height: 100 }} />
-                            <Text>{item.lastName}</Text>
-                           
-                        </View>
-                    )
-                })
-            }
-        </ScrollView>
-        
-    );
+        <>
+            <View>
+                <View style={{
+                    backgroundColor: '#08007C',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '100%',
 
+                }}>
+                    <FlatList
+                        numColumns={1}
+                        data={data}
+                        renderItem={({ item }) => (
+                            <TouchableOpacity key={item.id} onPress={() => cualquiera(item)} >
+                                <ScrollView
+                                    style={{
+                                        paddingHorizontal: 10,
+                                        borderRadius: 10,
+                                        marginHorizontal: 10,
+                                        marginVertical: 10,
+                                        backgroundColor: '#00B9E4',
+                                        borderRadius: 20,
+                                        width: 200,
+                                    }}>
+                                    <Image
+                                        id={item.name}
+                                        source={{ uri: item.image }}
+                                        resizeMode='contain'
+                                        style={{ resizeMode: 'cover', width: 100, height: 100, alignSelf: 'center', margin: 10, borderRadius: 20, justifyContent: 'center' }}
+                                    />
+                                    <Text
+                                        style={{ fontSize: 20, margin: 10, fontWeight: 'bold', alignSelf: 'center' }}
+                                    >{item.name}
+                                    </Text>
+                                </ScrollView>
+                            </TouchableOpacity>
+                        )}
+                    />
+                </View>
+            </View>
+        </>
+    )
 }
-
-export default Establishments;
-            
 
 
 
--- src/Service/Speciality.js
+++ src/Service/Speciality.js
@@ -3,7 +3,11 @@ import firestore from '@react-native-firebase/firestore';
 import Specialities from '../Pages/Specialities';
 import { View, Text, ScrollView, Image, FlatList, TouchableOpacity } from 'react-native';
 
-const ServiceSpecialities = ({ navigation }) => {
+const ServiceSpecialities = ({ navigation, route }) => {
+
+    const { user } = route.params
+
+    console.log("USER LLEGO A ESPECIALITY: ", user);
 
     const [DataSpecialities, setData] = useState([]);
 
@@ -24,19 +28,15 @@ const ServiceSpecialities = ({ navigation }) => {
     }, []);
 
     const cualquiera = (item) => {
-        navigation.navigate('Doctor', { Special: item });
+        navigation.navigate('Doctors', {speciality: item, user: user});
     }
 
     return (
         <>
             <View>
-                <Text
-                    style={{ fontSize: 35, fontWeight: 'bold', alignSelf: 'center', color: 'black' }}
-                >Specialities</Text>
                 <View
                     style={{
                         backgroundColor: '#08007C',
-                      
                         justifyContent: 'center',
                         alignItems: 'center',
                     
@@ -50,24 +50,18 @@ const ServiceSpecialities = ({ navigation }) => {
                                 <ScrollView
                                     style={{
                                         paddingHorizontal: 10,
-                                        paddingVertical: 10,
-                                        
                                         borderRadius: 10,
-
                                         marginHorizontal: 10,
                                         marginVertical: 10,
                                         backgroundColor: '#00B9E4',
                                         borderRadius: 20,
-                                        alignSelf: 'center',
-                                        elevation: 15,                                       
-                                        margin: 10,
-                                        resizeMode: 'cover'
+                                        width: 175,
                                     }}>
                                     <Image
                                         id={item.name}
                                         source={{ uri: item.image }}
                                         resizeMode='contain'
-                                        style={{ width: 100, height: 100, margin: 10, borderRadius: 20,  justifyContent: 'center' }}
+                                        style={{ resizeMode: 'cover', width: 100, height: 100, alignSelf: 'center', margin: 10, borderRadius: 20,  justifyContent: 'center' }}
                                     />
                                     <Text
                                         style={{ fontSize: 20, margin: 10, fontWeight: 'bold', alignSelf: 'center' }}
